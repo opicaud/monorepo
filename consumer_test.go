40ddef27ddd2b8f9ed5e009657e1b0544048f529
Olivier Picaud <opicaud.mailbox@gmail.com>
2022-11-21 12:25:55 +0100

    Chore: Testing first aera case with a grpc consumer

--- consumer_test.go
+++ consumer_test.go
@@ -1,29 +1,25 @@
 package consumer
 
 import (
+	"example2/utils"
 	"fmt"
-	"log"
 	"path/filepath"
 	"testing"
 
-	pactlog "github.com/pact-foundation/pact-go/v2/log"
 	message "github.com/pact-foundation/pact-go/v2/message/v4"
 	"github.com/stretchr/testify/assert"
 )
 
+var cp = &utils.ConsumerAndProvider{
+	Consumer: "grpc-consumer-go",
+	Provider: "area-calculator-provider",
+}
+
+var dir, _ = filepath.Abs("./proto/area_calculator.proto")
+
 func TestCalculateClient(t *testing.T) {
-	mockProvider, err := message.NewSynchronousPact(message.Config{
-		Consumer: "grpc-consumer-go",
-		Provider: "area-calculator-provider",
-	})
-	assert.NoError(t, err)
-
-	pactlog.SetLogLevel("TRACE")
-
-	dir, _ := filepath.Abs("./proto/area_calculator.proto")
-
 	grpcInteraction := `{
-		"pact:proto": "` + filepath.ToSlash(dir) + `",
+		"pact:proto": "` + dir + `",
 		"pact:proto-service": "Calculator/calculateMulti",
 		"pact:content-type": "application/protobuf",
 		"request": {
@@ -46,28 +42,19 @@ func TestCalculateClient(t *testing.T) {
 		}
 	}`
 
-	// Defined a new message interaction, and add the plugin config and the contents
-	err = mockProvider.
-		AddSynchronousMessage("calculate rectangle area request").
-		UsingPlugin(message.PluginConfig{
-			Plugin: "protobuf",
-		}).
-		WithContents(grpcInteraction, "application/grpc").
-		// Start the gRPC mock server
-		StartTransport("grpc", "127.0.0.1", nil).
-		// Execute the test
-		ExecuteTest(t, func(transport message.TransportConfig, m message.SynchronousMessage) error {
-			// Execute the gRPC client against the mock server
-			log.Println("Mock server is running on ", transport.Port)
-			area, err := GetRectangleAndSquareArea(fmt.Sprintf("localhost:%d", transport.Port))
+	var c = new(utils.ContractTest)
+	c.GrpcInteraction = grpcInteraction
+	c.Description = "calculate rectangle area request"
+	c.F = func(transport message.TransportConfig, m message.SynchronousMessage) error {
+		area, err := GetRectangleAndSquareArea(fmt.Sprintf("localhost:%d", transport.Port))
 
-			// Assert: check the result
-			assert.NoError(t, err)
-			assert.Equal(t, float32(12.0), area[0])
-			assert.Equal(t, float32(9.0), area[1])
+		assert.NoError(t, err)
+		assert.Equal(t, float32(12.0), area[0])
+		assert.Equal(t, float32(9.0), area[1])
 
-			return err
-		})
+		return err
+	}
 
-	assert.NoError(t, err)
+	utils.RunTest(t, *c, *cp)
+
 }
