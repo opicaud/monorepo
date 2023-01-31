899d352515f67db355408f48794d7749c06257ad
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-01-31 08:30:26 +0100

    refactor: move WORKSPACE file to create a real monorepo

--- shape-app/api/cmd/integration-test-with-pact.sh
+++ shape-app/api/cmd/integration-test-with-pact.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-./api/pacts/pacts_test_/pacts_test
-nohup ./api/cmd/cmd_/cmd &
+./shape-app/api/pacts/pacts_test_/pacts_test
+nohup ./shape-app/api/cmd/cmd_/cmd &
 echo $! > pid
-./external/pact-helper/verifier/pact_verifier -f api/pacts/pacts/grpc-consumer-go-area-calculator-provider.json --transport grpc -p 50051
+./external/pact-helper/verifier/pact_verifier -f shape-app/api/pacts/pacts/grpc-consumer-go-area-calculator-provider.json --transport grpc -p 50051
