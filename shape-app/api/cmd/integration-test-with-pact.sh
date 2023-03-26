484d34653013d3032377203936c7ee9cbf884a3f
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-03-26 23:04:59 +0200

    refactor: use pact-helper api to test consumer shape-app contract

--- shape-app/api/cmd/integration-test-with-pact.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-nohup ./shape-app/api/cmd/cmd_/cmd &
-echo $! > pid
-export PACT_PLUGIN_DIR=$(dirname "${RUNFILES_DIR}"/"${PACT_PLUGINS}")
- ./external/pact_bin/pact_verifier_cli -f shape-app/api/pacts/pacts/grpc-consumer-go-area-calculator-provider.json --transport grpc -p 50051
