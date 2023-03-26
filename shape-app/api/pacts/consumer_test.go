304feed683f736311bb3c2c0024e1a2d98468d48
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-03-26 21:37:15 +0200

    refactor: merge consumer_client and consumer_test

--- shape-app/api/pacts/consumer_test.go
+++ shape-app/api/pacts/consumer_test.go
@@ -1,12 +1,17 @@
 package pacts
 
 import (
+	"context"
 	"fmt"
 	"github.com/bazelbuild/rules_go/go/runfiles"
 	pact "github.com/opicaud/monorepo/pact-helper/go"
 	ac "github.com/opicaud/monorepo/shape-app/api/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
 	"os"
 	"testing"
+	"time"
 
 	message "github.com/pact-foundation/pact-go/v2/message/v4"
 	"github.com/stretchr/testify/assert"
@@ -44,7 +49,7 @@ func TestCreateShape(t *testing.T) {
 		shape := "rectangle"
 		rectangle := ac.ShapeMessage{Shape: shape, Dimensions: dimensions}
 		request := &ac.ShapeRequest{Shapes: &rectangle}
-		area, err := GetRectangleAndSquareArea(fmt.Sprintf("localhost:%d", transport.Port), request)
+		area, err := getRectangleAndSquareArea(fmt.Sprintf("localhost:%d", transport.Port), request)
 
 		assert.NoError(t, err)
 		assert.Equal(t, uint32(0), area.GetCode())
@@ -54,4 +59,26 @@ func TestCreateShape(t *testing.T) {
 
 	pact.RunTest(t, *c, *cp)
 
+}
+
+func getRectangleAndSquareArea(address string, request *ac.ShapeRequest) (*ac.Message, error) {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+		return nil, err
+	}
+	defer conn.Close()
+
+	c := ac.NewShapesClient(conn)
+
+	log.Println("Sending calculate rectangle and square request")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Create(ctx, request)
+
+	if err != nil {
+		return nil, err
+	}
+	return r.GetMessage(), nil
 }
