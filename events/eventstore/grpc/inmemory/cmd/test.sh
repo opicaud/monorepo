3f5a1dc33fb685878044105f558ea1796ff5be6f
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-04-06 23:24:35 +0200

    fix: use state manager to import events for provider tests

--- events/eventstore/grpc/inmemory/cmd/test.sh
+++ events/eventstore/grpc/inmemory/cmd/test.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-test=$1
-app=$3
-consumer_contract=$4
+consumer_contract=$3
+state_manager=$2
+event_store=$1
 
-./$test $app $consumer_contract
+nohup "$state_manager" &
+nohup "$event_store" &
+
+./external/pact_bin/pact_verifier_cli --state-change-url http://localhost:8080/event -f "${consumer_contract}" --transport grpc -p 50051
 
