00397a367f4cf7fa9525c4bf38bc3274c2a2ea27
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-04-09 18:45:57 +0200

    refactor: change test data to a real received event

--- pact-helper/go/statemanager/eventstore/main_test.go
+++ pact-helper/go/statemanager/eventstore/main_test.go
@@ -8,13 +8,13 @@ import (
 
 func TestShouldPplop(t *testing.T) {
 	var expected = make([]pkg.DomainEvent, 0)
-	expected = append(expected, NewDummyEvent("363c80b0-d2ae-11ed-afa1-0242ac120002", "name", []byte("plop")))
+	expected = append(expected, NewDummyEvent("00000000-0000-0000-0000-000000000000", "SHAPE_CREATED", []byte(`{"Nature":"square","Dimensions":[2,3],"Id":"00000000-0000-0000-0000-000000000000","Area":1}`)))
 
-	var request = `{"action": "setup","params": {"events": [{"id": "363c80b0-d2ae-11ed-afa1-0242ac120002", "name": "name", "data": "cGxvcA=="}]},"state": "expected State"}`
+	var request = `{"action": "setup","params": {"events":[{"aggregateId": {"id":"00000000-0000-0000-0000-000000000000"}, "name": "SHAPE_CREATED", "data": "eyJOYXR1cmUiOiJzcXVhcmUiLCJEaW1lbnNpb25zIjpbMiwzXSwiSWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAiLCJBcmVhIjoxfQ=="}]},"state":"a state"}`
 	parsedRequest := parseStringBody(request)
 
 	assert.Equal(t, "setup", parsedRequest.Action)
-	assert.Equal(t, "expected State", parsedRequest.State)
+	assert.Equal(t, "a state", parsedRequest.State)
 
 	assert.Len(t, parsedRequest.Params.Events, 1)
 	assert.Equal(t, expected[0].Name(), parsedRequest.Params.Events[0].Name())
