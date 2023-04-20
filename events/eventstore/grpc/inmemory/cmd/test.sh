e9f2e536f4dc24053281b27f36799d013166bda4
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-04-20 13:02:02 +0200

    refactor: put pact-provider test from run to test bazel context

--- events/eventstore/grpc/inmemory/cmd/test.sh
+++ events/eventstore/grpc/inmemory/cmd/test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
+export PACT_PLUGIN_DIR=$(dirname ${RUNFILES_DIR}/${PACT_PLUGIN_DIR})
 
 consumer_contract=$3
 state_manager=$2
 event_store=$1
 
+
 nohup "$state_manager" &
 nohup "$event_store" &
-
 ./external/pact_bin/pact_verifier_cli --state-change-url http://localhost:8080/event -f "${consumer_contract}" --transport grpc -p 50051
-
