39f125b7850775e56104f7aa7c969d77789abda8
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-05-02 21:54:10 +0200

    fix: increase delay to startup shape-app

--- shape-app/api/cmd/test.sh
+++ shape-app/api/cmd/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export PACT_PLUGIN_DIR=$(dirname ${RUNFILES_DIR}/${PACT_PLUGIN_DIR})
 nohup $1 &
-sleep 2
+sleep 10
 ./external/pact_bin/pact_verifier_cli -f "$2" --transport grpc -p 50051
