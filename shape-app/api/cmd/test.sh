c386b66f806695636de8b0d234a183769cefec9c
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-05-11 22:43:27 +0200

    refactor: use pact_plugin_zip instead of protobuf folder directly to use cache properly

--- shape-app/api/cmd/test.sh
+++ shape-app/api/cmd/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-export PACT_PLUGIN_DIR=$(dirname ${RUNFILES_DIR}/${PACT_PLUGIN_DIR})
+unzip -o pact-helper/pact_plugins_zip.zip
+export PACT_PLUGIN_DIR=$(dirname ${RUNFILES_DIR}/$(dirname ${PACT_PLUGIN_DIR}))
+
 nohup $1 &
 sleep 10
 ./external/pact_reference/pact_verifier_cli/pact_verifier_cli -f "$2" --transport grpc -p 50051
