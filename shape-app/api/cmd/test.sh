895247567e47ad5fdfe64c2ab17b5759ee9fc596
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-06-08 21:30:02 +0200

    refactor: decrease delay to start pact-verifier-cli

--- shape-app/api/cmd/test.sh
+++ shape-app/api/cmd/test.sh
@@ -3,5 +3,5 @@ unzip -o pact-helper/pact_plugins_zip.zip
 export PACT_PLUGIN_DIR=$(dirname ${RUNFILES_DIR}/$(dirname ${PACT_PLUGIN_DIR}))
 
 nohup $1 &
-sleep 10
+sleep 3
 ./external/pact_reference/pact_verifier_cli/pact_verifier_cli -f "$2" --transport grpc -p 50051
