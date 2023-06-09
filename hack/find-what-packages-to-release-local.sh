a767e12f3d0e4e5b6617cffc95b77d6a75c130a2
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-06-09 23:03:13 +0200

    refactor: improve find-what-packages-to-release to be able to run it locally

---
+++ hack/find-what-packages-to-release-local.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+toRelease=""
+branch=${1:-"main"}
+tags=$(git tag --sort=committerdate)
+lastTag=$(echo "$tags" | tail -n 1)
+lastTagRef=$lastTag
+changes=$(git diff --name-only "$lastTagRef" "$branch")
+if [ $? -ne 0 ]
+  then
+    exit 1
+fi
+for file in $changes; do
+  queried=$(bazel query --keep_going --noshow_progress "$file" --output=package 2>/dev/null)
+  if [ $? -eq 0 ]
+    then
+       package=$(echo "$queried" | cut -d '/' -f 1 | awk '{print "//"$1}')
+       hasBeenIdentified=$(echo "$toRelease" | grep "$package")
+       if [ "$hasBeenIdentified" = "" ] && [ "$package" != '//' ]
+         then
+           releaseTarget=$(bazel query --keep_going --noshow_progress "filter("release_me", kind("sh_binary", $package/...))" 2>/dev/null)
+           if [ "$releaseTarget" != "" ]
+           then
+              toRelease="$toRelease $package"
+           fi
+       fi
+       hasBeenIdentified=""
+  fi
+done
+
+echo "$toRelease"
--- hack/find-what-packages-to-release.sh
+++ hack/find-what-packages-to-release.sh
@@ -7,33 +7,5 @@ fi
 
 git clone --single-branch --branch main --quiet https://github.com/opicaud/monorepo.git
 cd monorepo
-
-toRelease=""
-branch="origin/main"
-tags=$(git tag --sort=committerdate)
-lastTag=$(echo "$tags" | tail -n 1)
-lastTagRef=$lastTag
-changes=$(git diff --name-only "$lastTagRef" "$branch")
-if [ $? -ne 0 ]
-  then
-    exit 1
-fi
-for file in $changes; do
-  queried=$(bazel query --keep_going --noshow_progress "$file" 2>/dev/null)
-  if [ $? -eq 0 ]
-    then
-       package=$(echo "$queried" | cut -d ':' -f 1)
-       hasBeenIdentified=$(echo "$toRelease" | grep "$package")
-       if [ "$hasBeenIdentified" = "" ] && [ "$package" != '//' ]
-         then
-           releaseTarget=$(bazel query --keep_going --noshow_progress "filter("release_me", kind("sh_binary", $package/...))" 2>/dev/null)
-           if [ "$releaseTarget" != "" ]
-           then
-              toRelease="$toRelease $package"
-           fi
-       fi
-       hasBeenIdentified=""
-  fi
-done
-
-echo "$toRelease"
+ls
+../hack/find-what-packages-to-release-local.sh "origin/main"
