096ce9ae252a239b415c27cd526d475f066c4a11
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-06-29 23:28:47 +0200

    feat: add a env variable with semver without v

--- hack/get-latest-tags.sh
+++ hack/get-latest-tags.sh
@@ -2,9 +2,11 @@
 
 getVersionOfApp () {
   firstapp=$(echo "$1" | head -n1)
-  version=v$(echo "$firstapp" | awk -F  '-v' '/1/ {print $2}')
+  semver=$(echo "$firstapp" | awk -F  '-v' '/1/ {print $2}')
+  version=v$semver
   app=$(echo "$firstapp"| awk -F  '-v' '/1/ {print $1}')
   echo "STABLE_$(echo "$app" | awk '{ print toupper($0) }' | sed 's/-/_/g') $version"
+  echo "STABLE_$(echo "$app" | awk '{ print toupper($0) }'| sed 's/-/_/g')_SEMVER $semver"
   tags=$(echo "$tags" | grep -v "$app")
   nb=$(echo "$tags" | wc -l)
   if [ "$nb" -gt "1" ]; then
