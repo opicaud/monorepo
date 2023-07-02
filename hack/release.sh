2222ad50e20c2e3a9c3d8d07a5aa15d3d7a680a8
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-07-02 16:22:24 +0200

    refactor: use current repo instead of cloning during release

--- hack/release.sh
+++ hack/release.sh
@@ -1,24 +1,18 @@
 #!/bin/sh
 
 runfiles_dir=$PWD
+export BAZEL_BINDIR=.
 path=$(dirname $1)
+
 echo "---Release of "$(dirname $1)"---"
 
-if [ -d "monorepo" ]
-then
-    echo "--> monorepo present, delete it"
-    rm -rf monorepo
-fi
-
-echo "--> cloning monorepo"
-git clone --single-branch --branch main --quiet https://github.com/opicaud/monorepo.git
-cd monorepo
 if [ -z "${GH_TOKEN}" ]
 then
   echo "--> GH_TOKEN not found, exiting now"
   exit 1
 else
   echo "--> GH_TOKEN found, continuing"
-  cd $path
-  GH_TOKEN=${GH_TOKEN} $runfiles_dir/hack/semantic_release_binary.sh
+  cd $BUILD_WORKSPACE_DIRECTORY/$path || exit 1
+  echo $runfiles_dir
+  GH_TOKEN=${GH_TOKEN} $runfiles_dir/hack/semantic_release_binary.sh --dry-run
 fi
