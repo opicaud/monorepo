930dca72e9d1112cdaa29a00faebe688ffb83a69
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-07-10 17:07:26 +0200

    feat: add release of untag yet component

--- hack/find-what-next-releases-are.sh
+++ hack/find-what-next-releases-are.sh
@@ -16,31 +16,21 @@ then
     rm -rf monorepo
 fi
 
-git clone --single-branch --branch main --quiet https://github.com/opicaud/monorepo.git
+git clone --single-branch --branch test --quiet https://github.com/opicaud/monorepo.git
 cd monorepo
-releaserc=$(cat << \EOF
-{
-  "branches": ["main"],
-  "extends": "semantic-release-monorepo",
-  "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/git",
-    "@semantic-release/github",
-    ["@semantic-release/exec", {
-        "analyzeCommitsCmd": "echo ${lastRelease.version} > current_release_version",
-        "verifyReleaseCmd": "echo ${nextRelease.version} > next_release_version",
-    }]
-  ]
-}
-EOF
-)
-echo "$releaserc" > .releaserc
 cd $path || exit 1
 
 $runfiles_dir/$1 --dry-run 1>/dev/null || exit 1
 echo "app=$(cat $runfiles_dir/$2 | sed 's/\"//g')"
-echo "currentVersion=$(cat current_release_version)"
+
+if [ -f current_release_version ]
+then
+  echo "currentVersion=$(cat current_release_version)"
+else
+  echo "currentVersion=not-available"
+  echo "nextVersion=1.0.0"
+  exit 0
+fi
 if [ -f next_release_version ]
 then
   echo "nextVersion=$(cat next_release_version)"
