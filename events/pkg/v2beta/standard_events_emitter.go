f82f92f8d39c57e4784d7f17de2d1f19224a35c2
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-12-26 23:01:28 +0100

    feat: use channel to send events in v2beta

---
+++ events/pkg/v2beta/standard_events_emitter.go
@@ -0,0 +1,42 @@
+package pkg
+
+import (
+	"github.com/opicaud/monorepo/events/pkg"
+	"sync"
+)
+
+type StandardEventsEmitter struct {
+	subscribers []Subscriber
+}
+
+func (s *StandardEventsEmitter) NotifyAll(event ...pkg.DomainEvent) {
+	eventsChn := make(chan []pkg.DomainEvent)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		eventsChn <- event
+	}()
+	go func() {
+		for _, subscriber := range s.subscribers {
+			if subscriber != nil {
+				subscriber.Update(eventsChn)
+			}
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+	close(eventsChn)
+}
+
+func (s *StandardEventsEmitter) Add(subscriber Subscriber) {
+	s.subscribers = append(s.subscribers, subscriber)
+}
+
+type EventsEmitter interface {
+	NotifyAll(event ...pkg.DomainEvent)
+	Add(subscriber Subscriber)
+}
+
+type Subscriber interface {
+	Update(eventsChn chan []pkg.DomainEvent)
+}
