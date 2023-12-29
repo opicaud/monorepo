df6b6058e0a3cd5705c15db6caecd95563dc6eb6
Olivier Picaud <opicaud.mailbox@gmail.com>
2023-12-29 14:09:07 +0100

    refactor: remove unecessary generated file current_release_version and next_release_version

--- hack/release.sh
+++ hack/release.sh
@@ -14,5 +14,4 @@ else
   echo "--> GH_TOKEN found, continuing"
   cd $BUILD_WORKSPACE_DIRECTORY/$path || exit 1
   GH_TOKEN=${GH_TOKEN} $runfiles_dir/hack/semantic_release_binary.sh
-  rm -f next_release_version
 fi
