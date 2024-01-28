b9bc82f15c6666ec551f98c0c3063393980181f8
Olivier Picaud <opicaud.mailbox@gmail.com>
2024-01-28 20:13:33 +0100

    feat: use default cli as oci image to run smoke tests from kubernetes cluster

--- shape-app/api/cli/main.go
+++ shape-app/api/cli/main.go
@@ -13,9 +13,23 @@ func main() {
 	cmd := &cli.Command{
 		Name:  "create-default-shape",
 		Usage: "make a rectangle ",
-		Action: func(context.Context, *cli.Command) error {
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "host",
+				Value: "localhost",
+				Usage: "host to run cli",
+			},
+			&cli.IntFlag{
+				Name:  "port",
+				Value: 50051,
+				Usage: "host port",
+			},
+		},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
 			f := cli2.Client{}
-			area, err := f.CreateShape(fmt.Sprintf("localhost:%d", 50051), CreateDefaultShapeRequest())
+			host := cmd.String("host")
+			port := cmd.Int("port")
+			area, err := f.CreateShape(fmt.Sprintf("%s:%d", host, port), CreateDefaultShapeRequest())
 			if err != nil {
 				fmt.Printf("%s\n", err)
 			}
