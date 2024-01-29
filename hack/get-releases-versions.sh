831c56e000dcd90073d2a85c3e29529be8cace87
Olivier Picaud <opicaud.mailbox@gmail.com>
2024-01-29 20:57:56 +0100

    fix: being able to release with deeper path

--- hack/get-releases-versions.sh
+++ hack/get-releases-versions.sh
@@ -4,8 +4,8 @@ apps=$(find -L bazel-bin -name next-version-to-release)
 for appFeature in $apps
 do
  . $appFeature
- echo "STABLE_$(echo "$app" | awk '{ print toupper($0) }' | sed 's/-/_/g')_NEXT_RELEASE_VERSION v$nextVersion"
- echo "STABLE_$(echo "$app" | awk '{ print toupper($0) }' | sed 's/-/_/g')_NEXT_RELEASE_SEMVER $nextVersion"
+ echo "STABLE_$(echo "$app" | awk '{ print toupper($0) }' | sed 's/-/_/g' | sed 's/\//_/g')_NEXT_RELEASE_VERSION v$nextVersion"
+ echo "STABLE_$(echo "$app" | awk '{ print toupper($0) }' | sed 's/-/_/g' | sed 's/\//_/g')_NEXT_RELEASE_SEMVER $nextVersion"
 
 done
 
