815eabf8bd528171c38eb6028077cf8f2b542bb7
Olivier Picaud <opicaud.mailbox@gmail.com>
2024-01-29 19:41:48 +0100

    Revert "fix: experiment volatile-status"
    This reverts commit f6675cf28c1f37ab2426fee3d4fa5a0e22bae6a9.

--- hack/get-releases-versions.sh
+++ hack/get-releases-versions.sh
@@ -6,9 +6,6 @@ do
  . $appFeature
  echo "STABLE_$(echo "$app" | awk '{ print toupper($0) }' | sed 's/-/_/g')_NEXT_RELEASE_VERSION v$nextVersion"
  echo "STABLE_$(echo "$app" | awk '{ print toupper($0) }' | sed 's/-/_/g')_NEXT_RELEASE_SEMVER $nextVersion"
- echo "$(echo "$app" | awk '{ print toupper($0) }' | sed 's/-/_/g')_NEXT_RELEASE_VERSION v$nextVersion"
- echo "$(echo "$app" | awk '{ print toupper($0) }' | sed 's/-/_/g')_NEXT_RELEASE_SEMVER $nextVersion"
-
 
 done
 
