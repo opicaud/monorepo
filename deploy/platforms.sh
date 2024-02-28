849208656bf2cacde8f11857c0f79b301720e9eb
Olivier Picaud <opicaud.mailbox@gmail.com>
2024-02-28 08:16:57 +0100

    feat: add security charts

--- deploy/platforms.sh
+++ deploy/platforms.sh
@@ -4,4 +4,5 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}
 argoPass=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 echo $argoPass
 brew install argocd
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.3/cert-manager.crds.yaml
 
