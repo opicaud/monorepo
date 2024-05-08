cbd7ad6b5441414b984f5f69787425a3fe588958
Olivier Picaud <opicaud.mailbox@gmail.com>
2024-05-08 07:54:12 +0200

    feat: install CRDs from helm charts to improve cohesion

--- deploy/platforms.sh
+++ deploy/platforms.sh
@@ -4,6 +4,5 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}
 argoPass=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 echo $argoPass
 brew install argocd
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.3/cert-manager.crds.yaml
 kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
 kubectl apply -f argocd-helm-creds.yaml
