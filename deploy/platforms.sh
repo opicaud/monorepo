cc6bb8c9ae7d7f914e39227170d870b963058f53
Olivier Picaud <opicaud.mailbox@gmail.com>
2024-05-20 12:49:21 +0200

    refactor: provide an observability-operator at cluster level

--- deploy/platforms.sh
+++ deploy/platforms.sh
@@ -4,6 +4,8 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}
 argoPass=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 echo $argoPass
 brew install argocd
-kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
 kubectl apply -f argocd-helm-creds.yaml
 bazel run //apps/security:security.install
+#Workaround, try to apply some priority to improve
+sleep 60
+bazel run //apps/observability-operator:observability_operator.install
